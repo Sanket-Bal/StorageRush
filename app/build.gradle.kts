9fa20a629ae9265e152dd9ea8600daf90961ce3f
Sanket Bal <sanketbrandset100cr@gmail.com>
2026-09-08 10:10:47 +0530

    version number 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.storagerush.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
